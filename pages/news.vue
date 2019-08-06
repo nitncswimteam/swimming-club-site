9a5ee96292e4011becc05ee6015f17c6a2ca3347
chige12 <tige1215.technic@gmail.com>
2019-08-06 12:13:12 +0900

    display news article

--- pages/news.vue
+++ pages/news.vue
@@ -1,9 +1,28 @@
 <template lang="pug">
+.news
+  .article(v-for="article in summarylist")
+    .article_title {{article.title}}
+
 </template>
 
 <script>
+import summaryJson from '~/contents/summary_news.json';
 export default {
   components: {
+  },
+  data(){
+    return {
+      summarylist: []
+    }
+  },
+  mounted(){
+    var summaryJsonMap = Object.entries(summaryJson.fileMap).map(([key, value]) => ({key, ...value}))
+    this.summarylist = summaryJsonMap.sort(function(a,b){
+      if(a.created_at < b.created_at) return -1;
+      if(a.created_at > b.created_at) return 1;
+      return 0;
+    });
+    //this.$store.commit('summarylist',this.summarylist)
   }
 }
 </script>
