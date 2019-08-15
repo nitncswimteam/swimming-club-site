a31766d9dfc679b6612abae0b77b9479792cf04b
chige12 <tige1215.technic@gmail.com>
2019-08-15 21:55:58 +0900

    add developers site

---
+++ pages/develop/index.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+.develop
+  .container
+</template>
+<script>
+export default {
+  layout: "develop"
+}
+</script>
+<style lang="scss">
+@import "~/assets/style/normalize.scss";
+@import "~/assets/style/variables.scss";
+@import "~/assets/style/mixin.scss";
+
+</style>
