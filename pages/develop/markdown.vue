2ae3d1284685b1f116b9cfb474be55c87c574955
chige12 <tige1215.technic@gmail.com>
2019-08-16 11:06:49 +0900

    develop page& markdown page

---
+++ pages/develop/markdown.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+.develop
+  .container
+    nuxt-link(to="/develop") トップに戻る
+    h1 MarkDownリアルタイム確認ツール
+    .top_text
+      p MarkDown形式の記事を、リアルタイムで表示確認しながら記述できるツールです。
+      
+
+</template>
+<script>
+export default {
+  layout: "develop"
+}
+</script>
+<style lang="scss" scoped>
+@import "~/assets/style/normalize.scss";
+@import "~/assets/style/variables.scss";
+@import "~/assets/style/mixin.scss";
+
+.top_text {
+  margin: 10px 0;
+  background: rgba($theme-blue,.1);
+  padding: 12px;
+  border-radius: 6px;
+  p { margin: .5rem 0; }
+}
+
+
+</style>
